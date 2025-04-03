f642092df6fde57203165675ffaa69bdd0d85978
Taneeshk Patel <taneeshkpatel@192.168.1.3>
2025-04-03 11:44:26 +0530

    aded whisper

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:200b8ff6e70932ca88756b9a4d8930cf207f5b2a66dfbd9f766e4f8627ae2a3d
-size 1568
+oid sha256:6d5949d8975efa68bf8fe3ac8521fd75b2c8c470487e14f69b5738151176face
+size 2630
